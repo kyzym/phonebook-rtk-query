8a3132e1fe59044c09518cd7c54ce1cf11d4b047
Denis Kyzym <102521716+kyzym@users.noreply.github.com>
2022-12-18 19:03:39 +0100

    added toaster and fixed not so disabled button

--- src/components/ContactList/ContactListItem/ContactsListItem.jsx
+++ src/components/ContactList/ContactListItem/ContactsListItem.jsx
@@ -2,21 +2,23 @@ import { BsPhone } from 'react-icons/bs';
 import { MdClose } from 'react-icons/md';
 import { DelButton, Name, Number } from './ContactsListItem.styled';
 
+import { toast } from 'react-hot-toast';
 import { useDeleteContactMutation } from 'redux/contactsSlice';
-// import { toast } from 'react-hot-toast';
 
 export const ContactsListItem = ({ contact: { name, phone, id } }) => {
-  const [deleteContact, { isLoading }] = useDeleteContactMutation();
+  const [deleteContact, { isLoading, isSuccess }] = useDeleteContactMutation();
 
-  // isLoading && toast.success(`OK, we've removed your contact`);
+  const handleDelete = () => {
+    deleteContact(id);
+    toast.success(`OK, we've removed your contact`);
+  };
 
   return (
     <>
       <BsPhone size={20} />
       <Name>{name}</Name>
       <Number>{phone}</Number>
-
-      <DelButton onClick={() => deleteContact(id)} disabled={isLoading}>
+      <DelButton onClick={handleDelete} disabled={isLoading || isSuccess}>
         <MdClose size={24} />
       </DelButton>
     </>
